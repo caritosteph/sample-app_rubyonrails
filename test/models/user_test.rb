7f2eaf1fdb887892c91a0ec6221f1e5bd681c489
Carolyn Ulfe Ureña <carito.ulfe@gmail.com>
2016-12-25 18:01:33 +0000

    Adding validation into Model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,11 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = User.new(name: "Example user", email: "user@gmail.com")
+  end
+  
+  test "should be valid" do
+    assert @user.valid?
+  end
 end
