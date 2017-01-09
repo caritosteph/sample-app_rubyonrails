7b25da1221dd38c855e2f6a60fb9a2be0a4b50cf
Carolyn <carito.ulfe@gmail.com>
2017-01-08 23:39:41 -0500

    redirect user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
-  
+
   def show
     @user = User.find(params[:id])
-    
+
   end
-  
+
   def new
     @user = User.new
   end
@@ -12,18 +12,18 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      
+      redirect_to @user
     else
       render 'new'
     end
   end
-  
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
-  
-  
-  
+
+
+
 end
