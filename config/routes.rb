8a2feaf2212916f5dfe4114d6400c127b6bc2c8d
Carolyn Ulfe Ureña <carito.ulfe@gmail.com>
2017-01-09 22:09:51 +0000

    Add session

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'session/new'
+
   root 'static_pages#home'
 
   get '/help', to: 'static_pages#help'
@@ -7,7 +9,11 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
   get '/signup', to: 'users#create'
-
+  
+  get '/login', to: 'session#new'
+  post '/login', to: 'session#create'
+  delete '/logout', to: 'session#destroy'
+  
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
