b8a884d59ee320de2848d53f31f1fe001c5e2383
Carolyn Ulfe Ureña <carito.ulfe@gmail.com>
2017-01-10 22:16:20 +0000

    adding login header

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -7,5 +7,8 @@ module SessionHelper
     def current_user
         @current_user ||= User.find_by(id: session[:user_id])
     end
-  
+    
+    def logged_in?
+        !@current_user.nil?
+    end
 end
