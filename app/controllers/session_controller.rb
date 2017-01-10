60204873d1e68cf7bf34fc7eb63b592aa2a6a786
Carolyn Ulfe Ureña <carito.ulfe@gmail.com>
2017-01-10 18:46:19 +0000

    Logging user valid

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -6,7 +6,8 @@ class SessionController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      render 'users/show'
+      log_in user
+      redirect_to user # user_url(user)
     else
       # Create an error message.
       flash.now[:danger] = "Invalid email/password combination"
