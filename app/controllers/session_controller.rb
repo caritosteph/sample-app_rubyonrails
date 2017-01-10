f795e160b5b32e93854a2260ff30fb6f1a8f1bea
Carolyn <carito.ulfe@gmail.com>
2017-01-10 02:42:36 -0500

    add authentication user

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,4 +1,15 @@
 class SessionController < ApplicationController
+
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      render 'user/show'
+    else
+      render 'new'
+    end
+  end
+
 end
