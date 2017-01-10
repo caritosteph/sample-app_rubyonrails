b18e1dbc935bdf0874ca98bb99686be5670278de
Carolyn Ulfe Ureña <carito.ulfe@gmail.com>
2017-01-10 15:17:46 +0000

    adding Login form

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,4 +1,16 @@
 class SessionController < ApplicationController
+  
   def new
   end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      render 'users/show'
+    else
+      # Create an error message.
+      render 'new'
+    end
+  end
+  
 end
