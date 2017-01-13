2d618187aa24ecf40217c030786b0346ab5a7ec9
Carolyn Ulfe Ureña <carito.ulfe@gmail.com>
2017-01-13 22:03:05 +0000

    create edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,11 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
   private
 
     def user_params
