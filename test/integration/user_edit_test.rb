860f2352b58f01caa7dba52ad8f1d70078a0cf28
Carolyn <carito.ulfe@gmail.com>
2017-01-16 00:28:03 -0500

    testing invalid edit user

---
+++ test/integration/user_edit_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class UserEditTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  def setup
+    @user = users(:michael)
+  end
+
+  test "unsuccessful edit user" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: { user: { name:  "",
+                                              email: "foo@invalid",
+                                              password:              "foo",
+                                              password_confirmation: "bar" } }
+    assert_template 'users/edit'
+    assert_select 'div#error_number'
+  end
+
+end
