35171a8c9f581be498d21cb264124abc6a61e18f
Carolyn <carito.ulfe@gmail.com>
2017-01-15 21:17:37 -0500

    updating with invalid user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,11 +18,20 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-  
+
   def edit
     @user = User.find(params[:id])
   end
-  
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+
+    else
+      render 'edit'
+    end
+  end
+
   private
 
     def user_params
