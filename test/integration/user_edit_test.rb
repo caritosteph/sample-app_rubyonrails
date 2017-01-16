1076219f410922ae005a4d1dd5ffc955c986848c
Carolyn <carito.ulfe@gmail.com>
2017-01-16 01:28:57 -0500

    Successful edit user

--- test/integration/user_edit_test.rb
+++ test/integration/user_edit_test.rb
@@ -19,4 +19,20 @@ class UserEditTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_number'
   end
 
+  test "successful edit user" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name:  name,
+                                              email: email,
+                                              password:              "",
+                                              password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
+
 end
