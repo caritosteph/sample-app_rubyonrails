bc8dce0fa70dd508fbb8384189be3e23e77e935f
Carolyn Ulfe Ureña <carito.ulfe@gmail.com>
2017-02-09 22:40:29 +0000

    Friendly forwarding

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -41,5 +41,14 @@ module SessionHelper
    session.delete(:user_id)
    @current_user = nil
  end
+ 
+ def redirect_back_or(default)
+  redirect_to(session[:forwarding_url] || default)
+  session.delete(:forwarding_url)
+ end
+ 
+ def store_location
+  session[:forwarding_url] = request.original_url if request.get?
+ end
 
 end
