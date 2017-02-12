984f48b4a0f8ff5daf74f5c793a7e9ca49c6079c
Carolyn <carito.ulfe@gmail.com>
2017-02-12 18:48:01 -0500

    deliting user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
   def show
     @user = User.find(params[:id])
@@ -26,6 +27,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
   def edit
     @user = User.find(params[:id])
   end
@@ -42,8 +49,12 @@ class UsersController < ApplicationController
 
   private
 
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin)
     end
 
     def logged_in_user
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -29,6 +29,16 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
 
+  # test "should not allow the admin attribute to be edited via the web" do
+  #   log_in_as(@other_user)
+  #   assert_not @other_user.admin?
+  #   patch user_path, params: {
+  #                     user: { password:              "foo",
+  #                             password_confirmation: "foo",
+  #                             admin: true } }
+  #   assert_not @other_user.FILL_IN.admin?
+  # end
+
   test "should redirect edit when logged in as wrong user" do
     log_in_as(@other_user)
     get edit_user_path(@user)
