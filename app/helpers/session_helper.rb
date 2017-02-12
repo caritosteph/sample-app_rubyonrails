7a9dec513ebd475d579b8ffe954ec0ab2ab5787f
Carolyn <carito.ulfe@gmail.com>
2017-02-11 23:17:45 -0500

    showing a list of users

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -10,10 +10,10 @@ module SessionHelper
    cookies.permanent[:remember_token] = user.remember_token
  end
 
- def current_user?
-  user == current_user
+ def current_user?(user)
+     user == current_user
  end
- 
+
  def current_user
    if (user_id = session[:user_id])
      @current_user ||= User.find_by(id: user_id)
@@ -41,12 +41,12 @@ module SessionHelper
    session.delete(:user_id)
    @current_user = nil
  end
- 
+
  def redirect_back_or(default)
   redirect_to(session[:forwarding_url] || default)
   session.delete(:forwarding_url)
  end
- 
+
  def store_location
   session[:forwarding_url] = request.original_url if request.get?
  end
